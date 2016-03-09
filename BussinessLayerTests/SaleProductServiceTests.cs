68221a29ef8fae1e94aa4261e37e126038ccb2a3
allenchen <allenchen@nine-yi.com>
2016-03-09 23:37:03 +0800

    Initialization

---
+++ BussinessLayerTests/SaleProductServiceTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExpectedObjects;
+
+namespace BusinessLayer.Tests
+{
+    [TestClass()]
+    public class SaleProductServiceTests
+    {
+        [TestMethod()]
+        public void GetSaleProduct_GroupColumn_Cost_GroupCount_3()
+        {
+            //// Arrange
+            var saleProduct = new SaleProductService();
+            var groupColumn = "Cost";
+            var groupCount = 3;
+
+            var expected = new List<int> { 6, 15, 24, 21 };
+
+            //// Act
+            var actual = saleProduct.GetSaleProductDataGroupSum(groupColumn, groupCount);
+
+            //// Assert
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
+
+        [TestMethod()]
+        public void GetSaleProduct_GroupColumn_Revenue_GroupCount_4()
+        {
+            //// Arrange
+            var saleProduct = new SaleProductService();
+            var groupColumn = "Revenue";
+            var groupCount = 4;
+
+            var expected = new List<int> { 50, 66, 60 };
+
+            //// Act
+            var actual = saleProduct.GetSaleProductDataGroupSum(groupColumn, groupCount);
+
+            //// Assert
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
+    }
+}
---
+++ HomeWork_Day1/SaleProductService.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class SaleProductService
+    {
+        public List<int> GetSaleProductDataGroupSum(string groupColumn, int groupCount)
+        {
+            var saleProductList = this.GetSaleProductList();
+
+            var groupList = new List<int>();
+            var groupSum = 0;
+            var countIndex = 0;
+
+            foreach (var item in saleProductList)
+            {
+                Type type = item.GetType();
+                var property = type.GetProperty(groupColumn);
+                var value = (int)property.GetValue(item);
+                groupSum += value;
+
+                countIndex++;
+
+                if (countIndex % groupCount == 0)
+                {
+                    groupList.Add(groupSum);
+                    groupSum = 0;
+                    countIndex = 0;
+                }
+            }
+
+            //// 剩餘資料自成一組
+            if (groupSum > 0)
+            {
+                groupList.Add(groupSum);
+            }
+
+            return groupList;
+        }
+
+        private List<SaleProductListEntity> GetSaleProductList()
+        {
+            var saleProductList = new List<SaleProductListEntity>();
+            saleProductList.Add(new SaleProductListEntity { Id = 1, Cost = 1, Revenue = 11, SellPrice = 21 });
+            saleProductList.Add(new SaleProductListEntity { Id = 2, Cost = 2, Revenue = 12, SellPrice = 22 });
+            saleProductList.Add(new SaleProductListEntity { Id = 3, Cost = 3, Revenue = 13, SellPrice = 23 });
+            saleProductList.Add(new SaleProductListEntity { Id = 4, Cost = 4, Revenue = 14, SellPrice = 24 });
+            saleProductList.Add(new SaleProductListEntity { Id = 5, Cost = 5, Revenue = 15, SellPrice = 25 });
+            saleProductList.Add(new SaleProductListEntity { Id = 6, Cost = 6, Revenue = 16, SellPrice = 26 });
+            saleProductList.Add(new SaleProductListEntity { Id = 7, Cost = 7, Revenue = 17, SellPrice = 27 });
+            saleProductList.Add(new SaleProductListEntity { Id = 8, Cost = 8, Revenue = 18, SellPrice = 28 });
+            saleProductList.Add(new SaleProductListEntity { Id = 9, Cost = 9, Revenue = 19, SellPrice = 29 });
+            saleProductList.Add(new SaleProductListEntity { Id = 10, Cost = 10, Revenue = 20, SellPrice = 30 });
+            saleProductList.Add(new SaleProductListEntity { Id = 11, Cost = 11, Revenue = 21, SellPrice = 31 });
+
+            return saleProductList;
+        }
+    }
+
+    class SaleProductListEntity
+    {
+        public int Id { get; set; }
+
+        public int Cost { get; set; }
+
+        public int Revenue { get; set; }
+
+        public int SellPrice { get; set; }
+    }
+}
